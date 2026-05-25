d986b11eb5ccfac868dad40a35ee8fbe90f8bbc5
Lechenco <glechenco@gmail.com>
2026-05-25 16:19:43 -0300

    feat: lsp config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.config")
+
+      configs.setup({
+          ensure_installed = {
+              "lua",
+				"python",
+				"go",
+				"sql",
+				"make",
+				"dockerfile",
+				"yaml",
+				"graphql",
+				"terraform",
+				"html",
+          },
+          sync_install = false,
+          auto_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
+}
