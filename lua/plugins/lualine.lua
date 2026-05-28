cdf570bf5ddc5e06beaa06979a7d237fc3f2b1cd
Lechenco <glechenco@gmail.com>
2026-05-28 15:26:56 -0300

    plugins: clear plugins and add barbar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,22 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require("lualine").setup({
-            options = {
-                section_separators = { left = '', right = '' },
-                sections = {
-                    lualine_a = {
-                        { 'mode', separator = { left = '' }, right_padding = 2 },
-                    },
-                    lualine_b = { 'filename', 'branch' },
-                    lualine_c = { 'fileformat' },
-                    lualine_x = {},
-                    lualine_y = { 'filetype', 'progress' },
-                    lualine_z = {
-                        { 'location', separator = { right = '' }, left_padding = 2 },
-                    },
-                }
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				sections = {
+					lualine_a = {
+						{ "mode", right_padding = 2 },
+					},
+					lualine_b = { "filename", "branch" },
+					lualine_c = { "fileformat" },
+					lualine_x = {},
+					lualine_y = { "filetype", "progress" },
+					lualine_z = {
+						{ "location", left_padding = 2 },
+					},
+				},
+			},
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,21 @@
 -- plugins/telescope.lua:
 return {
-    'nvim-telescope/telescope.nvim', --tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 
-         'nvim-lua/plenary.nvim' ,
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        "nvim-tree/nvim-web-devicons",
-      },
+	"nvim-telescope/telescope.nvim", --tag = '0.1.8',
+	-- or                              , branch = '0.1.x',
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
 
-      config = function()
-          local telescope = require("telescope")
+	config = function()
+		local telescope = require("telescope")
 
-        -- set keymaps
-         vim.keymap.set('n', '<C-p>', "<cmd>Telescope find_files<cr>", {})
-         vim.keymap.set('n', '<Space><Space>', "<cmd>Telescope old_files<cr>", {})
-         vim.keymap.set('n', '<Space>fg', "<cmd>Telescope live_grep<cr>", {})
-         vim.keymap.set('n', '<Space>fh', "<cmd>Telescope help_tags<cr>", {})
-      end
-    }
+		telescope.setup({})
+		-- set keymaps
+		vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", {})
+		vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope oldfiles<cr>", {})
+		vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {})
+		vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {})
+	end,
+}
